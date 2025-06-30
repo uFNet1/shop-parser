8fcea0112f61df9a14865b539ee118760844123c
uFNet1 <91597674+uFNet1@users.noreply.github.com>
2025-06-30 13:58:59 +0300

    refactor and change formatting

---
+++ src/bot/formatUtils.ts
@@ -0,0 +1,136 @@
+import { CardMessage, CardPhoto } from "../types";
+
+export function createItemCard(
+  name: string,
+  // description: string,
+  price: number,
+  cardPrice: number | null,
+  oldPrice: number | null
+): CardMessage {
+  if (cardPrice === null && oldPrice === null) {
+    return {
+      text:
+        `🛒 *${escapeMarkdown(name)}*\n` +
+        `💰 Ціна: *${formatNumber(price)} грн*\n`,
+      options: {
+        parse_mode: "MarkdownV2",
+      },
+    };
+  } else if (typeof cardPrice === "number" && typeof oldPrice === "number") {
+    return {
+      text:
+        `🛒 *${escapeMarkdown(name)}*\n` +
+        `💰 Ціна: *${formatNumber(price)} грн* \\(~${formatNumber(
+          oldPrice
+        )} грн~\\)\n` +
+        `💳 З картою АТБ: *${formatNumber(cardPrice)} грн*\n`,
+      options: {
+        parse_mode: "MarkdownV2",
+      },
+    };
+  } else if (typeof cardPrice === "number" && oldPrice === null) {
+    return {
+      text:
+        `🛒 *${escapeMarkdown(name)}*\n` +
+        `💰 Ціна: *${formatNumber(price)} грн*\n` +
+        `💳 З картою АТБ: *${formatNumber(cardPrice)} грн*\n`,
+      options: {
+        parse_mode: "MarkdownV2",
+      },
+    };
+  } else if (cardPrice === null && typeof oldPrice === "number") {
+    return {
+      text:
+        `🛒 *${escapeMarkdown(name)}*\n` +
+        `💰 Ціна: *${formatNumber(price)} грн* \\(~${formatNumber(
+          oldPrice
+        )} грн~\\)\n`,
+      options: {
+        parse_mode: "MarkdownV2",
+      },
+    };
+  } else {
+    return {
+      text:
+        `🛒 *${escapeMarkdown(name)}*\n` +
+        `💰 Ціна: *${formatNumber(price)} грн*\n`,
+      options: {
+        parse_mode: "MarkdownV2",
+      },
+    };
+  }
+}
+
+export function createItemCardPhoto(
+  photoUrl: string,
+  name: string,
+  // description: string,
+  price: number,
+  cardPrice: number | null,
+  oldPrice: number | null
+): CardPhoto {
+  if (cardPrice === null && oldPrice === null) {
+    return {
+      photo: photoUrl,
+      caption:
+        `🛒 *${escapeMarkdown(name)}*\n` +
+        `💰 Ціна: *${formatNumber(price)} грн*\n`,
+      options: {
+        parse_mode: "MarkdownV2",
+      },
+    };
+  } else if (typeof cardPrice === "number" && typeof oldPrice === "number") {
+    return {
+      photo: photoUrl,
+      caption:
+        `🛒 *${escapeMarkdown(name)}*\n` +
+        `💰 Ціна: *${formatNumber(price)} грн* \\(~${formatNumber(
+          oldPrice
+        )} грн~\\)\n` +
+        `💳 З картою АТБ: *${formatNumber(cardPrice)} грн*\n`,
+      options: {
+        parse_mode: "MarkdownV2",
+      },
+    };
+  } else if (typeof cardPrice === "number" && oldPrice === null) {
+    return {
+      photo: photoUrl,
+      caption:
+        `🛒 *${escapeMarkdown(name)}*\n` +
+        `💰 Ціна: *${formatNumber(price)} грн*\n` +
+        `💳 З картою АТБ: *${formatNumber(cardPrice)} грн*\n`,
+      options: {
+        parse_mode: "MarkdownV2",
+      },
+    };
+  } else if (cardPrice === null && typeof oldPrice === "number") {
+    return {
+      photo: photoUrl,
+      caption:
+        `🛒 *${escapeMarkdown(name)}*\n` +
+        `💰 Ціна: *${formatNumber(price)} грн* \\(~${formatNumber(
+          oldPrice
+        )} грн~\\)\n`,
+      options: {
+        parse_mode: "MarkdownV2",
+      },
+    };
+  } else {
+    return {
+      photo: photoUrl,
+      caption:
+        `🛒 *${escapeMarkdown(name)}*\n` +
+        `💰 Ціна: *${formatNumber(price)} грн*\n`,
+      options: {
+        parse_mode: "MarkdownV2",
+      },
+    };
+  }
+}
+
+function escapeMarkdown(text: string): string {
+  return text.replace(/([_*[\]()~`>#+\-=|{}.!\\])/g, "\\$1");
+}
+function formatNumber(v: number) {
+  return v.toFixed(2).replace(".", "\\.");
+}
--- src/types.ts
+++ src/types.ts
@@ -6,3 +6,12 @@ export interface ItemDataModel {
   itemPhoto: string | null;
   itemLink: string;
 }
+export interface CardMessage {
+  text: string;
+  options: { parse_mode: "MarkdownV2" };
+}
+export interface CardPhoto {
+  photo: string;
+  caption: string;
+  options: { parse_mode: "MarkdownV2" };
+}
